a35a2efb92d6433ed0f0255105328f08925dc32f
Brian Matzelle <bmatzel1@binghamton.edu>
2024-12-06 02:53:23 -0500

    added background blur

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { A } from "./ui/a";
+import { Card } from "./ui/card";
 
 export default function Project({
   title,
@@ -15,20 +15,25 @@ export default function Project({
   return (
     <>
       <div className="flex gap-2 items-start justify-between">
-        <div className="flex flex-col gap-1">
-          <A href={href}>{title}</A>
-          {children}
-        </div>
-        {src && (
-          <A
+        <div className="flex flex-col gap-1 w-full">
+          <div className="flex w-full justify-between items-center">
+            <A href={href}>{title}</A>
+            <A
             variant="source"
-            href={src}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            (source)
-          </A>
-        )}
+              href={src ?? "#"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="backdrop-blur-sm text-center rounded-sm w-16 outline outline-1 outline-dotted outline-black/[.08] dark:outline-white/[.145] flex items-center justify-center"
+            >
+              {src ? (
+                <span>src</span>
+              ) : (
+                <span className="text-rose-400/80 hover:underline">n/a</span>
+              )}
+            </A>
+          </div>
+          <Card>{children}</Card>
+        </div>
       </div>
       <div className="border-b border-black/[.08] dark:border-white/[.145]"></div>
     </>
